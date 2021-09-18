2ad4c44c61b0ebd11b87675549fb27862fac1b5c
dragon <myth19950803@gmail.com>
2021-09-18 13:28:12 -0700

    validation form

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,16 @@
     <header>
       <div class="container">
         <div class="wrapper">
-          <FormTrial />
+          <div class="row">
+            <div class="col-sm-5">
+              <FormTrial />
+            </div>
+          </div>
         </div>
       </div>
     </header>
     <div class="container">
-      <PartnersList />
+      <CompaniesList />
     </div>
     <Footer />
   </div>
@@ -18,25 +22,27 @@
 <script>
 import Menu from "~/components/Menu.vue";
 import FormTrial from "~/components/FormTrial.vue";
-import PartnersList from "~/components/PartnersList.vue";
+import CompaniesList from "~/components/CompaniesList.vue";
 import Footer from "~/components/Footer.vue";
 
 export default {
   components: {
     Menu,
     FormTrial,
-    PartnersList,
+    CompaniesList,
     Footer
   }
 };
 </script>
 <style lang="scss" scoped>
 header {
-  min-height: 700px;
   background-image: url("/illo@2X.png");
   background-repeat: no-repeat;
   background-size: 70% auto;
   background-position: 100% bottom;
+  @include breakpoint(mobileonly) {
+    background-image: inherit;
+  }
   .wrapper {
     padding-top: 200px;
     padding-bottom: 190px;
